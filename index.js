6d901ac6dbc3f7802b944d1d153cb92f79a86406
Harshit Mourya <mouryaharshit03@gmail.com>
2024-06-13 12:21:04 +0530

    first commit to simple_form

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+const port = 8080;
+const path = require("path");
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.set(express.static(path.join(__dirname, "public")));
+
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(port, () => {
+  console.log(`Listening to port ${port}...`);
+});
+
+app.get("/", (req, res) => {
+  res.render("home.ejs");
+});
+let data;
+app.post("/newuser", (req, res) => {
+  //   console.log(req.body);
+  let { user, email } = req.body;
+  data = { user: user, email: email };
+  res.render("info.ejs", { data });
+});
+
+app.get("/edit", (req, res) => {
+  //   console.log(data);
+  res.render("edit.ejs", { data });
+});
